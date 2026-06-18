fb043011e4c90ad21e65fb33be07db7048e62d1c
arulzzzxd <arulzz.xd@gmail.com>
2026-06-19 02:03:12 +0700

    Update fakestoryig.js

--- api/tools/fakestoryig.js
+++ api/tools/fakestoryig.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const axios = require("axios");
-const { createCanvas, loadImage } = require("@napi-rs/canvas");
+const { createCanvas, loadImage } = require("canvas");
 
 const router = express.Router();
 
