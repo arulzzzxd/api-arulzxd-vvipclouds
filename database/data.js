8ebd2b7d06e3236506685bd5817f285b8cbd99e8
arulzzzxd <arulzz.xd@gmail.com>
2026-06-29 04:53:54 +0700

    Add initial data structure in data.js

---
+++ database/data.js
@@ -0,0 +1,10 @@
+const data = [
+    {
+    "id": 1780765874079,
+    "title": "Contoh2",
+    "language": "javascript",
+    "desc": "By arulzxd",
+    "date": "7 Jun 2026",
+    "content": "Contoh3"
+}
+];
