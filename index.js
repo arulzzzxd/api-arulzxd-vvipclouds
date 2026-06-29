c67228d2d6a3eb32b2b30a436bc285fed66d7d06
arulzzzxd <arulzz.xd@gmail.com>
2026-06-29 19:08:01 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -83,27 +83,6 @@ const playlist = [
   }
 ];
 
-// Tambahkan ini di bagian paling awal fungsi request handler di index.js kamu
-export default async function handler(req, res) {
-    // Setel CORS Headers secara manual agar diizinkan oleh browser
-    res.setHeader('Access-Control-Allow-Credentials', true);
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
-    res.setHeader(
-        'Access-Control-Allow-Headers',
-        'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-    );
-
-    // Tangani preflight request browser (OPTIONS) agar tidak error/stuck
-    if (req.method === 'OPTIONS') {
-        res.status(200).end();
-        return;
-    }
-
-    // ... sisa kode index.js kamu yang sudah ada di bawahnya ...
-}
-
-
 app.get('/uploader', (req, res) => {
   res.sendFile(path.join(__dirname, 'uploader.html'));
 });
